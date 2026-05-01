3990729cd45de0e8ab132d60c887e69ffc73d1ca
Wendy Contreras Martinez <wcontrerasmartinez@ucsb.edu>
2026-04-30 17:26:08 -0700

    Fix duplicate UCSBDiningCommonsMenuItem form import

---
+++ frontend/src/fixtures/ucsbDiningCommonsMenuItemFixtures.js
@@ -0,0 +1,31 @@
+const ucsbDiningCommonsMenuItemFixtures = {
+  oneMenuItem: {
+    id: 1,
+    diningCommonsCode: "ortega",
+    name: "Baked Pesto Pasta with Chicken",
+    station: "Entree Specials",
+  },
+
+  threeMenuItems: [
+    {
+      id: 1,
+      diningCommonsCode: "ortega",
+      name: "Baked Pesto Pasta with Chicken",
+      station: "Entree Specials",
+    },
+    {
+      id: 2,
+      diningCommonsCode: "ortega",
+      name: "Tofu Banh Mi Sandwich (v)",
+      station: "Entree Specials",
+    },
+    {
+      id: 3,
+      diningCommonsCode: "portola",
+      name: "Cream of Broccoli Soup (v)",
+      station: "Greens & Grains",
+    },
+  ],
+};
+
+export { ucsbDiningCommonsMenuItemFixtures };
