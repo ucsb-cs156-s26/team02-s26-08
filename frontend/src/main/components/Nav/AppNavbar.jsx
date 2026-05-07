9ab0c01ff59708e61a2ab32f2d495792b51dc5e2
Ria Singh <riasingh@ucsb.edu>
2026-05-06 19:51:31 -0700

    last changes

--- frontend/src/main/components/Nav/AppNavbar.jsx
+++ frontend/src/main/components/Nav/AppNavbar.jsx
@@ -1,116 +1,3 @@
-// import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-// import { Link } from "react-router";
-// import { hasRole } from "main/utils/useCurrentUser";
-// import AppNavbarLocalhost from "main/components/Nav/AppNavbarLocalhost";
-
-// export default function AppNavbar({
-//   currentUser,
-//   systemInfo,
-//   doLogout,
-//   currentUrl = window.location.href,
-// }) {
-//   var oauthLogin = systemInfo?.oauthLogin || "/oauth2/authorization/google";
-
-//   return (
-//     <>
-//       {(currentUrl.startsWith("http://localhost:3000") ||
-//         currentUrl.startsWith("http://127.0.0.1:3000")) && (
-//         <AppNavbarLocalhost url={currentUrl} />
-//       )}
-
-//       <Navbar
-//         expand="xl"
-//         variant="dark"
-//         bg="dark"
-//         sticky="top"
-//         data-testid="AppNavbar"
-//       >
-//         <Container>
-//           <Navbar.Brand as={Link} to="/">
-//             team02
-//           </Navbar.Brand>
-
-//           <Navbar.Toggle />
-
-//           <Nav className="me-auto">
-//             {systemInfo?.springH2ConsoleEnabled && (
-//               <>
-//                 <Nav.Link href="/h2-console">H2Console</Nav.Link>
-//               </>
-//             )}
-//             {systemInfo?.showSwaggerUILink && (
-//               <>
-//                 <Nav.Link href="/swagger-ui/index.html">Swagger</Nav.Link>
-//               </>
-//             )}
-//           </Nav>
-
-//           <>
-//             {/* be sure that each NavDropdown has a unique id and data-testid  */}
-//           </>
-
-//           <Navbar.Collapse className="justify-content-between">
-//             <Nav className="mr-auto">
-//               {hasRole(currentUser, "ROLE_ADMIN") && (
-//                 <NavDropdown
-//                   title="Admin"
-//                   id="appnavbar-admin-dropdown"
-//                   data-testid="appnavbar-admin-dropdown"
-//                 >
-//                   <NavDropdown.Item href="/admin/users">Users</NavDropdown.Item>
-//                 </NavDropdown>
-//               )}
-//               {currentUser && currentUser.loggedIn ? (
-//                 <>
-//                   <Nav.Link as={Link} to="/articles">
-//                     Articles
-//                   </Nav.Link>
-//                   <Nav.Link as={Link} to="/restaurants">
-//                     Restaurants
-//                   </Nav.Link>
-//                   <Nav.Link as={Link} to="/ucsbdates">
-//                     UCSB Dates
-//                   </Nav.Link>
-//                   <Nav.Link as={Link} to="/diningcommonsmenuitem">
-//                     UCSBDiningCommonsMenuItem
-//                   </Nav.Link>
-//                   <Nav.Link as={Link} to="/recommendationrequests">
-//                     RecommendationRequests
-//                   </Nav.Link>
-//                   <Nav.Link as={Link} to="/menuitemreview">
-//                     Menu Item Review
-//                   </Nav.Link>
-//                   <Nav.Link as={Link} to="/helprequest">
-//                     HelpRequest
-//                   </Nav.Link>
-//                   <Nav.Link as={Link} to="/ucsborganization">
-//                     UCSBOrganization
-//                   </Nav.Link>
-//                 </>
-//               ) : (
-//                 <></>
-//               )}
-//             </Nav>
-
-//             <Nav className="ml-auto">
-//               {currentUser && currentUser.loggedIn ? (
-//                 <>
-//                   <Navbar.Text className="me-3" as={Link} to="/profile">
-//                     Welcome, {currentUser.root.user.email}
-//                   </Navbar.Text>
-//                   <Button onClick={doLogout}>Log Out</Button>
-//                 </>
-//               ) : (
-//                 <Button href={oauthLogin}>Log In</Button>
-//               )}
-//             </Nav>
-//           </Navbar.Collapse>
-//         </Container>
-//       </Navbar>
-//     </>
-//   );
-// }
-
 import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { Link } from "react-router";
 import { hasRole } from "main/utils/useCurrentUser";
@@ -123,12 +10,14 @@ export default function AppNavbar({
   currentUrl = window.location.href,
 }) {
   var oauthLogin = systemInfo?.oauthLogin || "/oauth2/authorization/google";
+
   return (
     <>
       {(currentUrl.startsWith("http://localhost:3000") ||
         currentUrl.startsWith("http://127.0.0.1:3000")) && (
         <AppNavbarLocalhost url={currentUrl} />
       )}
+
       <Navbar
         expand="xl"
         variant="dark"
@@ -140,15 +29,28 @@ export default function AppNavbar({
           <Navbar.Brand as={Link} to="/">
             team02
           </Navbar.Brand>
+
           <Navbar.Toggle />
+
+          <Nav className="me-auto">
+            {systemInfo?.springH2ConsoleEnabled && (
+              <>
+                <Nav.Link href="/h2-console">H2Console</Nav.Link>
+              </>
+            )}
+            {systemInfo?.showSwaggerUILink && (
+              <>
+                <Nav.Link href="/swagger-ui/index.html">Swagger</Nav.Link>
+              </>
+            )}
+          </Nav>
+
+          <>
+            {/* be sure that each NavDropdown has a unique id and data-testid  */}
+          </>
+
           <Navbar.Collapse className="justify-content-between">
             <Nav className="mr-auto">
-              {systemInfo?.springH2ConsoleEnabled && (
-                <Nav.Link href="/h2-console">H2Console</Nav.Link>
-              )}
-              {systemInfo?.showSwaggerUILink && (
-                <Nav.Link href="/swagger-ui/index.html">Swagger</Nav.Link>
-              )}
               {hasRole(currentUser, "ROLE_ADMIN") && (
                 <NavDropdown
                   title="Admin"
@@ -189,6 +91,7 @@ export default function AppNavbar({
                 <></>
               )}
             </Nav>
+
             <Nav className="ml-auto">
               {currentUser && currentUser.loggedIn ? (
                 <>
